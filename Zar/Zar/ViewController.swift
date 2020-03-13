19a7e22aed63618a83144407f63758b94a9e99ab
hcd <huseyincandayan@Cans-MacBook-Pro.local>
2020-03-13 11:26:29 +0300

    First

---
+++ Zar/Zar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zar
+//
+//  Created by Huseyin Can Dayan on 6.02.2020.
+//  Copyright © 2020 Huseyin Can Dayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
